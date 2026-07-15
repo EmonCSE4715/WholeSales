25fbfd19755caf090c7847d8fe224160e7fa1ee1
EmonCSE4715 <asadulislamemon661@gmail.com>
2026-07-15 22:39:26 +0600

    fix mongodb hostname

--- wholesale-server/index.js
+++ wholesale-server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.evn2ej1.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ykfzpf.mongodb.net/?retryWrites=true&w=majority`;
 
 
 
